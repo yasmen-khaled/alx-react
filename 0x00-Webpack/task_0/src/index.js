ad20acafc40d65e3cc358d23a8573fcabf436068
yasmen <yasmen.khaled2000@gmail.com>
2024-05-21 01:45:20 +0200

    o

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,7 +0,0 @@
-// interesting jquery import thanks to babel
-import $ from 'jquery';
-
-// add three different p tags to body
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
