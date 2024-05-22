38554b4de8a98a8a3fa7533c3809a56393fca773
yasmen <yasmen.khaled2000@gmail.com>
2024-05-22 22:49:34 +0200

    done

--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -1,11 +1,17 @@
 function getFullYear() {
-	  return new Date().getFullYear();
+	return new Date().getFullYear();
 }
 
 function getFooterCopy(isIndex) {
-	if (!isIndex) return "Holberton School main dashboard";
-	return "Holberton School";
+  if (!isIndex) return "Holberton School main dashboard";
+  return "Holberton School";
+}
+
+function getLatestNotification() {
+  return {
+	  __html: '<strong>Urgent requirement</strong> - complete by EOD'
+  }
 }
 
 // export
-export { getFullYear, getFooterCopy };
+export { getFullYear, getFooterCopy, getLatestNotification };
