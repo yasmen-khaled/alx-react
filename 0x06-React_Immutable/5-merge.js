f9824534dba04a23c6a6bd0add7f5bbfec761396
yasmen <yasmen.khaled2000@gmail.com>
2024-06-14 00:16:31 +0200

    done

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -5,6 +5,9 @@ function concatElements(page1, page2) {
   //
   // page1 - array
   // page2 - array
+  //
+  // Returns a list containing the values of page1 and page2
+  const myList = Immutable.List(page1);
   const myList2 = Immutable.List(page2);
   return myList.concat(myList2);
 }
@@ -15,6 +18,8 @@ function mergeElements(page1, page2) {
   // page1 - object
   // page2 - object
   //
+  // Returns a list containing the values of page1 and page2
+  // if two values are the same, page2 value will be used.
   const myMap = Immutable.Map(page1);
   const myMap2 = Immutable.Map(page2);
   return myMap.merge(myMap2);
